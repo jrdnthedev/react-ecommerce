18256b89531616379aa9538e8e29fcb02774d12b
andre mcmorris <ajmcmorris@gmail.com>
2025-02-10 20:27:36 -0500

    enhancements

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -11,8 +11,6 @@ export const store = configureStore({
   },
 });
 
-// Infer the type of `store`
 export type AppStore = typeof store;
 export type RootState = ReturnType<AppStore["getState"]>;
-// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore["dispatch"];
