ee46b3f5bb38ecd850b576918abc8a2e6fa0ed89
andre mcmorris <ajmcmorris@gmail.com>
2025-02-10 20:44:12 -0500

    grid enhancements

--- app/components/product-list/product-list.tsx
+++ app/components/product-list/product-list.tsx
@@ -11,7 +11,7 @@ export function ProductList() {
     if (error) return <p>Error: {error}</p>;
     return (
         <div className="p-4">
-            <div className=" grid grid-cols-4 gap-4">
+            <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 {products.map((product: any) => {
                     return (
                         <ProductCard key={product.id} item={product} />
