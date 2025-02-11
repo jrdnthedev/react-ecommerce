17b4f36574fb99f28926f622b4cde11ed45a1120
andre mcmorris <ajmcmorris@gmail.com>
2025-02-10 20:29:24 -0500

    updates

--- app/(pages)/cart/page.tsx
+++ app/(pages)/cart/page.tsx
@@ -3,7 +3,6 @@ import { CartList } from "@/app/components/cart-list/cart-list";
 export default function Cart() {
     return (
         <div className="p-8 ">
-            <h1>Cart</h1>
             <CartList />
         </div>
     )
--- app/components/cart-list/cart-list.tsx
+++ app/components/cart-list/cart-list.tsx
@@ -12,7 +12,7 @@ export function CartList() {
         dispatch(removeFromCart(id));
     }
     return (
-        <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <div className="flex items-center justify-between mb-4">
                 <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Items</h5>
             </div>
