6a06280cb085d61b858ee289c41dc13a06278555
andre mcmorris <ajmcmorris@gmail.com>
2025-02-13 18:26:58 -0500

    added test

---
+++ app/components/product-list/product-list.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { ProductList } from "./product-list";
+
+describe('ProductCard Component', () => {
+    const productList = [
+        {
+            category: "men's clothing",
+            description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            id: 1,
+            image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            price: 109.95,
+            rating: { rate: 3.9, count: 120 },
+            title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+        },
+        {
+            category: "men's clothing",
+            description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            id: 2,
+            image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            price: 19.95,
+            rating: { rate: 4.9, count: 10 },
+            title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+        }
+    ]
+
+    it('should display product card', () => {
+        const store = configureStore({ reducer: { product: (state = { products: productList, loading: false, error: null }, action) => state } })
+        render(
+            <Provider store={store}>
+                <ProductList />
+            </Provider>
+        );
+        const productCard = screen.getAllByTestId('product-card');
+        expect(productCard).toHaveLength(2);
+    });
+});
