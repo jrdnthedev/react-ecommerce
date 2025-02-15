b6f2ebfd17bf52166c9e8889a3f112dedfa04955
andre mcmorris <ajmcmorris@gmail.com>
2025-02-15 14:04:12 -0500

    small code enhancements

--- app/(pages)/success/page.tsx
+++ app/(pages)/success/page.tsx
@@ -1,13 +1,9 @@
-import { useRouter } from "next/router";
-
+"use client";
 export default function SuccessfulPurchase() {
-    const router = useRouter();
-    const { session_id } = router.query;
     return (
         <div>
             <h1>Payment Successful</h1>
             <p>Thank you for your purchase!</p>
-            <p>Session ID: {session_id}</p>
         </div>
     );
 }
