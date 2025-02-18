77c6521db25492e6b33c4d9bf9f475d5e28fbbd3
andre mcmorris <ajmcmorris@gmail.com>
2025-02-17 20:45:19 -0500

    added middleware to support auth functionality

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// 1. Specify protected and public routes
+const protectedRoutes = ["/products", "/cart", "/checkout"];
+const publicRoutes = ["/login", "/signup", "/"];
+
+export function middleware(req: NextRequest) {
+  console.log("Middleware running on:", req.nextUrl);
+  console.log("Token from cookies:", req.cookies.get("token")?.value);
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  // 3. Decrypt the session from the cookie
+  const token = req.cookies.get("token")?.value;
+
+  // 4. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/signin", req.nextUrl));
+  }
+
+  // 5. Redirect to /products if the user is authenticated
+  if (isPublicRoute && token && !req.nextUrl.pathname.startsWith("/products")) {
+    return NextResponse.redirect(new URL("/products", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
