678132e5f2c0cf46cab4fb3407c34f44b0876732
andre mcmorris <ajmcmorris@gmail.com>
2025-02-17 20:47:30 -0500

    updated user model

---
+++ app/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
+const UserSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+export default mongoose.models.User ||
+  mongoose.model<IUser>("User", UserSchema);
