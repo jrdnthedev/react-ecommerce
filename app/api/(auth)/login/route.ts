79aa2a311e718946c40ccd449b2649ab4556aefb
andre mcmorris <ajmcmorris@gmail.com>
2025-02-17 20:45:44 -0500

    updated route logic

---
+++ app/api/(auth)/login/route.ts
@@ -0,0 +1,55 @@
+import User from "@/app/models/User";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { connectToDatabase } from "@/app/lib/mongodb";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectToDatabase();
+    const { email, password } = await req.json();
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: `User does not exist` },
+        { status: 400 }
+      );
+    }
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(password, salt);
+
+    const isPassWordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPassWordMatch) {
+      return NextResponse.json(
+        { message: `Password does not match` },
+        { status: 401 }
+      );
+    }
+
+    const tokenData = {
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    const response = NextResponse.json(
+      { message: `User found successfully`, success: true, data: token },
+      { status: 200 }
+    );
+
+    response.cookies.set({
+      name: "token",
+      value: token,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+    });
+    return response;
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
---
+++ app/api/(auth)/signup/route.ts
@@ -0,0 +1,42 @@
+import { connectToDatabase } from "@/app/lib/mongodb";
+import User from "@/app/models/User";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    await connectToDatabase();
+    const { email, password, firstName, lastName } = await req.json();
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: `User with email ${email} already exists` },
+        { status: 400 }
+      );
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    try {
+      await newUser.save();
+    } catch (err: any) {
+      console.error("Error saving user:", err.message);
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+
+    return NextResponse.json(
+      { message: `User created successfully`, success: true, data: email },
+      { status: 200 }
+    );
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
