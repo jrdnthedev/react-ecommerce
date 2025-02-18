683a88546d93f25676950d7fffec9ff3917934af
andre mcmorris <ajmcmorris@gmail.com>
2025-02-17 20:48:31 -0500

    a11y fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Tengo Store</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
