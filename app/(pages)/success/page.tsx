6fc3a20ae40d5bc0abb04ac2b455705f4f464e65
andre mcmorris <ajmcmorris@gmail.com>
2025-02-19 09:42:09 -0500

    updated payment information pages

--- app/(pages)/success/page.tsx
+++ app/(pages)/success/page.tsx
@@ -5,10 +5,14 @@ export default function SuccessfulPurchase() {
     const searchParams = useSearchParams();
     const session_id = searchParams.get('session_id');
     return (
-        <div>
-            <h1>Payment Successful</h1>
-            <p>Thank you for your purchase!</p>
-            <p>Session ID: {session_id}</p>
-        </div>
+
+
+        <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Purchase Successful</h1>
+                <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Thanks for your purchase! Here at Tengo we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            </div>
+        </section>
+
     );
 }
