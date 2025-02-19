61e67d3786c63fd0339194c8c1cbf3e30e8859a6
andre mcmorris <ajmcmorris@gmail.com>
2025-02-19 08:48:15 -0500

    small updates

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -35,7 +35,7 @@ export function Navbar() {
         <nav className="fixed w-full z-20 top-0 start-0 border-b border-gray-400 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href='/' className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Tengo Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tengo</span>
                 </Link>
                 <button onClick={handleToggle} data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded={isOpen}>
