97455f17bf0a53b1b65a18432a1f96213728396a
andre mcmorris <ajmcmorris@gmail.com>
2025-02-24 07:35:35 -0500

    updated cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,12 @@
 describe("app flow", () => {
   it("should add product to cart and checkout", () => {
     cy.visit("http://localhost:3000/");
+    cy.contains("Sign In").click();
+    cy.url().should("include", "/signin");
+    cy.get('input[name="email"]').type("test@example.com");
+    cy.get('input[name="password"]').type("Passw0rd");
+    cy.get('button[type="submit"]').click();
+    cy.url().should("include", "/products");
     cy.contains("Products").click();
     cy.url().should("include", "/products");
     cy.contains("Add to cart").click();
