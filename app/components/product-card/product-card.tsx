41ce7e9ae35a944415b138eefa05b9f45e1276f4
andre mcmorris <ajmcmorris@gmail.com>
2025-02-23 19:08:29 -0500

    added toast for handlecart

--- app/components/product-card/product-card.tsx
+++ app/components/product-card/product-card.tsx
@@ -1,25 +1,37 @@
 "use client";
 
-import { useAppDispatch } from "@/app/hooks";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import { addToCart } from "@/app/redux/slices/cartSlice";
-import { AppDispatch } from "@/app/redux/store";
+import { AppDispatch, RootState } from "@/app/redux/store";
 import { Product } from "@/app/types/types";
 import Image from "next/image";
 import { StarRating } from "../star-rating/star-rating";
 import { PriceFormatter } from "@/app/utils/price-formatter";
 import { ToastContainer, toast } from 'react-toastify';
+import { set } from "mongoose";
 interface CardProps {
     item: Product;
 }
 
 export function ProductCard({ item }: CardProps) {
     const dispatch = useAppDispatch<AppDispatch>();
+    const cart = useAppSelector((state: RootState) => state.cart.cart ?? []);
 
-    const handleAddToCart = () => {
-        dispatch(addToCart(item));
-        toast('Item added to cart', {
-            position: "bottom-right", autoClose: 2000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined,
-        });
+    const handleAddToCart = (item: Product) => {
+        const itemInCart = cart.some((cartItem) => cartItem.id === item.id);
+        console.log(itemInCart);
+        if (!itemInCart) {
+            dispatch(addToCart(item));
+            setTimeout(() => {
+                toast('Item added to cart', {
+                    position: "bottom-right", autoClose: 2000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined,
+                });
+            }, 100);
+        } else {
+            toast('Item already in cart', {
+                position: "bottom-right", autoClose: 2000, hideProgressBar: false, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined,
+            });
+        }
     }
 
     return (
@@ -37,7 +49,7 @@ export function ProductCard({ item }: CardProps) {
                 <div className="flex items-center justify-between">
                     <span className="text-3xl font-bold text-gray-900 dark:text-white">{PriceFormatter(item.price)}</span>
                     <button
-                        onClick={handleAddToCart}
+                        onClick={() => handleAddToCart(item)}
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
                 </div>
             </div>
