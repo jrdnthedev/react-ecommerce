fdcf043b21dd8ee5da33a42c924766d02b28ddaf
andre mcmorris <ajmcmorris@gmail.com>
2025-02-23 21:52:27 -0500

    added loading skeleton to product page

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -1,9 +1,11 @@
 import { ProductList } from "@/app/components/product-list/product-list";
+import { LoadingSpinnerSkeleton } from "@/app/ui/skeleton";
+import { Suspense } from 'react';
 
 export default function Product() {
     return (
-        <>
+        <Suspense fallback={<LoadingSpinnerSkeleton />}>
             <ProductList />
-        </>
+        </Suspense>
     );
 }
--- app/components/product-list/product-list.tsx
+++ app/components/product-list/product-list.tsx
@@ -3,8 +3,6 @@ import { useAppSelector } from "@/app/hooks";
 import { RootState } from "@/app/redux/store";
 import { ProductCard } from "../product-card/product-card";
 import { Product } from "@/app/types/types";
-import { LoadingSpinnerSkeleton } from "@/app/ui/skeleton";
-import { Suspense } from 'react';
 import { InfoModal } from "../info-modal/info-modal";
 
 export function ProductList() {
@@ -13,15 +11,13 @@ export function ProductList() {
     if (error) return <InfoModal data={error} />;
     console.log(products);
     return (
-        <Suspense fallback={<LoadingSpinnerSkeleton />}>
-            <ul role="list" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                {products.map((product: Product) => {
-                    return (
-                        <li role="listitem" key={product.id}><ProductCard key={product.id} item={product} /></li>
-                    );
-                })}
-            </ul>
-        </Suspense>
+        <ul role="list" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {products.map((product: Product) => {
+                return (
+                    <li role="listitem" key={product.id}><ProductCard key={product.id} item={product} /></li>
+                );
+            })}
+        </ul>
     );
 }
 
